41ff811a4b24d2a562c33c39e7d626c292fe7b33
nickchahley <nickchahley@gmail.com>
2019-07-31 11:25:14 -0400

    Add stdout

--- symlink_nb.py
+++ symlink_nb.py
@@ -4,6 +4,12 @@ subdir (./nb) for more convinient annotation. The txt file should contain a
 single file ID (eg 01, 04-2) on each line. Run this script from directory
 containing the images.
 
+Example file contents:
+
+05
+10
+11
+
 Useage:
     nb.py nb_file_ids.txt
 """
@@ -29,6 +35,7 @@ def force_symlink(src, dst):
         if e.errno == errno.EEXIST:
             os.remove(dst)
             os.symlink(src, dst)
+            return e
 
 def main():
     args = parse_args()
@@ -48,7 +55,12 @@ def main():
         for im in glob(i+glob_suffix):
             src = os.path.abspath(im)
             dst = pj(ps(src)[0], copy_dir, im)
-            force_symlink(src, dst)
+            e = force_symlink(src, dst)
+
+    if e:
+        print('Overwrote prexisting symlinks!')
+
+    print('Symlinked IDs in %s to %s' %(args.infile, args.linkdir))
 
 
 main()
